70e3a6b8e09407879f334704d7afe3f6513ff00b
Fityan <fityannugroho@gmail.com>
2022-11-08 19:59:10 +0700

    feat: set color palette

--- resources/scripts/Pages/Welcome.tsx
+++ resources/scripts/Pages/Welcome.tsx
@@ -2,7 +2,7 @@ import AppHead from '@/Components/AppHead';
 import ColorModeContext from '@/Utils/ColorModeContext';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
-import { IconButton } from '@mui/material';
+import { Button, IconButton } from '@mui/material';
 import React from 'react';
 
 export default function Welcome() {
@@ -21,6 +21,20 @@ export default function Welcome() {
       >
         {colorMode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
       </IconButton>
+      <div className="flex gap-1 flex-wrap">
+        <Button variant="contained">Primary</Button>
+        <Button variant="outlined">Primary</Button>
+        <Button variant="contained" color="secondary">Secondary</Button>
+        <Button variant="outlined" color="secondary">Secondary</Button>
+        <Button variant="contained" color="info">Info</Button>
+        <Button variant="outlined" color="info">Info</Button>
+        <Button variant="contained" color="success">Success</Button>
+        <Button variant="outlined" color="success">Success</Button>
+        <Button variant="contained" color="warning">Warning</Button>
+        <Button variant="outlined" color="warning">Warning</Button>
+        <Button variant="contained" color="error">Error</Button>
+        <Button variant="outlined" color="error">Error</Button>
+      </div>
     </>
   );
 }
--- resources/scripts/Utils/AppThemeOptions.ts
+++ resources/scripts/Utils/AppThemeOptions.ts
@@ -11,9 +11,33 @@ const AppThemeOptions = (colorMode: PaletteMode): ThemeOptions => ({
     ...(colorMode === 'light'
       ? {
         // palette values for light mode
+        primary: {
+          main: '#0066cc',
+          light: '#5c93ff',
+          dark: '#003d9a',
+          contrastText: '#ffffff',
+        },
+        secondary: {
+          main: '#0000cc',
+          light: '#633aff',
+          dark: '#000099',
+          contrastText: '#ffffff',
+        },
       }
       : {
         // palette values for dark mode
+        primary: {
+          main: '#0098ff',
+          light: '#69c8ff',
+          dark: '#006bcb',
+          contrastText: '#000000',
+        },
+        secondary: {
+          main: '#6128e2',
+          light: '#9a59ff',
+          dark: '#1400af',
+          contrastText: '#ffffff',
+        },
       }
     ),
   },
