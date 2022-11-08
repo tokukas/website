0f2e4ad38831211a739cf5d10689b5f5ff010ee9
Fityan <fityannugroho@gmail.com>
2022-11-08 07:31:23 +0700

    setup mui-tailwind integration

--- resources/scripts/app.tsx
+++ resources/scripts/app.tsx
@@ -1,11 +1,12 @@
-import './bootstrap';
 import '../styles/app.css';
+import './bootstrap';
 
-import React from 'react';
-import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/inertia-react';
 import { InertiaProgress } from '@inertiajs/progress';
+import { StyledEngineProvider } from '@mui/material';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import AppConfig from './Config/App';
 
 const appName = AppConfig.name;
@@ -23,8 +24,12 @@ await createInertiaApp({
   setup({ el, App, props }) {
     const root = createRoot(el);
 
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    root.render(<App {...props} />);
+    root.render(
+      <StyledEngineProvider injectFirst>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <App {...props} />
+      </StyledEngineProvider>,
+    );
   },
 });
 
