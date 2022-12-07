4a9b54c2b25fd577f3ba592c93d2fe2e4120517b
Fityan <fityannugroho@gmail.com>
2022-12-07 11:49:12 +0700

    fix: MenuItemLink href using keyboard input MenuItemLink href not responding to keyboard input, but works with mouse click

--- resources/scripts/Components/MenuItemLink.tsx
+++ resources/scripts/Components/MenuItemLink.tsx
@@ -2,9 +2,7 @@ import Link from '@mui/material/Link';
 import MenuItem, { MenuItemProps } from '@mui/material/MenuItem';
 import React from 'react';
 
-export type TPropsMenuItemLink = MenuItemProps & {
-  href?: string;
-};
+export type TPropsMenuItemLink = MenuItemProps<'a'>;
 
 /**
  * The [Link](https://mui.com/material-ui/api/link) variant of the
@@ -18,25 +16,12 @@ export default function MenuItemLink({
 }: TPropsMenuItemLink) {
   return (
     <MenuItem
+      component={Link}
+      href={href}
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...props}
     >
-      <Link
-        href={href}
-        underline="none"
-        color="inherit"
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          width: '100%',
-        }}
-      >
-        {children}
-      </Link>
+      {children}
     </MenuItem>
   );
 }
-
-MenuItemLink.defaultProps = {
-  href: undefined,
-};
