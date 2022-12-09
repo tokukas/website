927d66f7062f445fed7e707fbe0096ac5ed99241
Fityan <fityannugroho@gmail.com>
2022-12-09 07:09:30 +0700

    fix: remove dummy content in home page

--- resources/scripts/Pages/Welcome.tsx
+++ resources/scripts/Pages/Welcome.tsx
@@ -1,15 +1,9 @@
 import AppHead from '@/Components/AppHead';
 import Navbar from '@/Components/Navbar';
 import BaseLayout from '@/Layouts/BaseLayout';
-import ColorModeContext from '@/Utils/ColorModeContext';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
 import * as React from 'react';
 
 export default function Welcome() {
-  const { colorMode, toggleColorMode } = React.useContext(ColorModeContext);
   return (
     <BaseLayout>
       <AppHead
@@ -18,27 +12,6 @@ export default function Welcome() {
           dengan harga terjangkau. #YangBekasPastiLebihMurah"
       />
       <Navbar />
-      <IconButton
-        sx={{ ml: 1 }}
-        onClick={toggleColorMode}
-        color="inherit"
-      >
-        {colorMode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-      </IconButton>
-      <div className="flex gap-1 flex-wrap">
-        <Button variant="contained">Primary</Button>
-        <Button variant="outlined">Primary</Button>
-        <Button variant="contained" color="secondary">Secondary</Button>
-        <Button variant="outlined" color="secondary">Secondary</Button>
-        <Button variant="contained" color="info">Info</Button>
-        <Button variant="outlined" color="info">Info</Button>
-        <Button variant="contained" color="success">Success</Button>
-        <Button variant="outlined" color="success">Success</Button>
-        <Button variant="contained" color="warning">Warning</Button>
-        <Button variant="outlined" color="warning">Warning</Button>
-        <Button variant="contained" color="error">Error</Button>
-        <Button variant="outlined" color="error">Error</Button>
-      </div>
     </BaseLayout>
   );
 }
