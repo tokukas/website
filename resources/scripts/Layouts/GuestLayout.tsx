72ed6e79fc4947c3ca68ca9e3c547c3c8a4facec
Fityan <fityannugroho@gmail.com>
2022-12-16 20:39:36 +0700

    feat: add theme toggle in `GuestLayout`

--- resources/scripts/Layouts/GuestLayout.tsx
+++ resources/scripts/Layouts/GuestLayout.tsx
@@ -1,8 +1,14 @@
 import BrandLogo from '@/Components/BrandLogo';
 import Link from '@/Components/Link';
+import ColorModeContext from '@/Utils/ColorModeContext';
+import Brightness4 from '@mui/icons-material/Brightness4';
+import Brightness7 from '@mui/icons-material/Brightness7';
 import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
 import Paper from '@mui/material/Paper';
-import React from 'react';
+import Tooltip from '@mui/material/Tooltip';
+import Zoom from '@mui/material/Zoom';
+import * as React from 'react';
 import BaseLayout from './BaseLayout';
 
 type TPropsGuestLayout = {
@@ -10,6 +16,8 @@ type TPropsGuestLayout = {
 }
 
 export default function Guest({ children }: TPropsGuestLayout) {
+  const { colorMode, toggleColorMode } = React.useContext(ColorModeContext);
+
   return (
     <BaseLayout>
       <Box
@@ -24,6 +32,26 @@ export default function Guest({ children }: TPropsGuestLayout) {
           alignItems: 'center',
         }}
       >
+        <Tooltip
+          title={`Switch to ${colorMode === 'light' ? 'dark' : 'light'} mode`}
+          placement="left"
+        >
+          <Zoom in>
+            <Fab
+              color="primary"
+              aria-label="Theme Toggle"
+              size="small"
+              sx={{
+                position: 'fixed',
+                top: 16,
+                right: 16,
+              }}
+              onClick={toggleColorMode}
+            >
+              {colorMode === 'light' ? <Brightness4 /> : <Brightness7 />}
+            </Fab>
+          </Zoom>
+        </Tooltip>
         <Paper
           className="w-full max-w-md"
           sx={{
