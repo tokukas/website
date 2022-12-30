fc26c71f5cf56e461a5e4eaebaf1c668d309eff7
Fityan <fityannugroho@gmail.com>
2022-12-30 18:28:31 +0700

    feat: add `Sidebar` in `DashboardLayout`

--- resources/scripts/Layouts/DashboardLayout.tsx
+++ resources/scripts/Layouts/DashboardLayout.tsx
@@ -1,5 +1,6 @@
 import Navbar from '@/Components/Navbar';
-import SettingsMenu from '@/Components/NavMenuItems/SettingsMenu';
+import Sidebar from '@/Components/Sidebar';
+import Box from '@mui/material/Box';
 import * as React from 'react';
 import BaseLayout from './BaseLayout';
 
@@ -8,15 +9,26 @@ export type TPropsDashboardLayout = {
 }
 
 export default function DashboardLayout({ children }: TPropsDashboardLayout) {
+  const [open, setOpen] = React.useState(false);
+
   return (
     <BaseLayout>
-      <Navbar
-        withoutNavItems
-        setMainUserMenus={() => [
-          SettingsMenu,
-        ]}
-      />
-      <main>{children}</main>
+      <Navbar withoutNavItems />
+      <Box sx={{ display: 'flex', mt: '70px' }}>
+        <Sidebar
+          open={open}
+          width={240}
+          top={70}
+          sx={{ zIndex: (theme) => theme.zIndex.appBar - 1 }}
+          setOpen={setOpen}
+          menuItems={[
+            //
+          ]}
+        />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          {children}
+        </Box>
+      </Box>
     </BaseLayout>
   );
 }
