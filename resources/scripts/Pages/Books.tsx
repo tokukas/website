f49bc26fd913b77086b01c3fb2b4b6f02f04315e
Fityan <fityannugroho@gmail.com>
2023-02-19 16:50:05 +0700

    feat(books): add books table

--- resources/scripts/Pages/Books.tsx
+++ resources/scripts/Pages/Books.tsx
@@ -1,10 +1,46 @@
+import { Book } from '@/Entities/Book';
 import DashboardLayout from '@/Layouts/DashboardLayout';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import React from 'react';
 
-export default function Books() {
+export type TPropsBooks = {
+  books: Book[];
+}
+
+export default function Books({ books }: TPropsBooks) {
+  const bookColumns: GridColDef<Book>[] = [
+    { field: 'id', headerName: 'ID', width: 80 },
+    { field: 'title', headerName: 'Title', width: 240 },
+    { field: 'publisher_id', headerName: 'Publisher ID' },
+    { field: 'year_published', headerName: 'Year Published', width: 80 },
+    { field: 'language_code', headerName: 'Language', width: 80 },
+    { field: 'width', headerName: 'Width', width: 80 },
+    { field: 'height', headerName: 'Height', width: 80 },
+    { field: 'weight', headerName: 'Weight', width: 80 },
+    { field: 'num_of_pages', headerName: 'Pages', width: 80 },
+    { field: 'isbn', headerName: 'ISBN' },
+    { field: 'description', headerName: 'Description' },
+    { field: 'created_at', headerName: 'Created At' },
+    { field: 'updated_at', headerName: 'Updated At' },
+  ];
+
   return (
     <>
-      Books
+      <Typography variant="h4" component="h1" gutterBottom>
+        Books
+      </Typography>
+
+      <Paper sx={{ height: 380, width: '100%' }}>
+        <DataGrid
+          columns={bookColumns}
+          rows={books}
+          pageSize={5}
+          checkboxSelection
+          disableSelectionOnClick
+        />
+      </Paper>
     </>
   );
 }
