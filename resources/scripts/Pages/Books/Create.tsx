ee1c010c96d589a0d21983139dfe31e19589e439
Fityan <fityannugroho@gmail.com>
2023-02-23 08:45:13 +0700

    feat(books): use Autocomplete for Language field Due to bad performance if using Select

--- resources/scripts/Pages/Books/Create.tsx
+++ resources/scripts/Pages/Books/Create.tsx
@@ -8,7 +8,6 @@ import Autocomplete from '@mui/material/Autocomplete';
 import Box from '@mui/material/Box';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import InputAdornment from '@mui/material/InputAdornment';
-import MenuItem from '@mui/material/MenuItem';
 import TextField from '@mui/material/TextField';
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
@@ -78,28 +77,22 @@ export default function AddBook({ publishers, categories }: TPropsAddBook) {
           )}
         />
 
-        <TextField
-          select
-          label="Language"
-          name="language_code"
-          required
-          defaultValue="en"
-          SelectProps={{
-            MenuProps: {
-              MenuListProps: {
-                sx: {
-                  maxHeight: 300,
-                },
-              },
-            },
-          }}
-        >
-          {Language.getAllLanguages().map((lang) => (
-            <MenuItem key={lang.code} value={lang.code}>
-              {`${lang.name} - ${lang.native}`}
-            </MenuItem>
-          ))}
-        </TextField>
+        <Autocomplete
+          id="language"
+          options={Language.getAllLanguages()}
+          getOptionLabel={(option) => `${option.name} - ${option.native}`}
+          isOptionEqualToValue={(option, value) => option.code === value.code}
+          renderInput={(params) => (
+            <TextField
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              {...params}
+              label="Language"
+              name="language"
+              required
+              placeholder="Select a language"
+            />
+          )}
+        />
 
         <Autocomplete
           id="category"
