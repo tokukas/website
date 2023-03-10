9c9414b1fbc4d412bffd05ef3e8a82d03519e0a0
Fityan <fityannugroho@gmail.com>
2023-03-10 22:47:36 +0700

    fix: redirect `to` param now can receive url

--- app/Http/Middleware/RedirectTo.php
+++ app/Http/Middleware/RedirectTo.php
@@ -4,13 +4,13 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Exception\RouteNotFoundException;
 
 /**
- * Middleware to set the intended url to specific **route name** or **previous url** that defined in `to` request param.
+ * Middleware to set the intended url to specific **route name** or **specific url** that defined in `to` request param.
  *
- * Set `to` with `"back"` to set the intended url with previous url.
+ * Set `to` with `"back"` to set the intended url with **previous url**.
  *
  * If `to` is present but the value is invalid, the intended url will not be set.
  */
@@ -23,14 +23,17 @@ class RedirectTo
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if ($request->has('to')) {
-            $target = $request->input('to');
+        if ($target = $request->input('to')) {
             $intendedUrl = null;
 
             try {
-                $intendedUrl = route($target);
-            } catch (RouteNotFoundException $th) {
-                if ($target === 'back') {
+                // Ensure the target is a registered route.
+                Route::getRoutes()->match(request()->create($target));
+                $intendedUrl = $target;
+            } catch (\Throwable $th) {
+                if (Route::has($target)) {
+                    $intendedUrl = route($target);
+                } else if ($target === 'back') {
                     $intendedUrl = url()->previous();
                 }
             }
