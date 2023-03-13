fcf8b972572a31321917133850f4402539b76264
Fityan <fityannugroho@gmail.com>
2023-03-13 10:24:16 +0700

    feat(show-product): full screen photo view with options

--- resources/scripts/Pages/Products/Show.tsx
+++ resources/scripts/Pages/Products/Show.tsx
@@ -1,12 +1,23 @@
 import AppHead from '@/Components/AppHead';
 import Link from '@/Components/Link';
 import VerticalTable from '@/Components/VerticalTable';
+import { Image } from '@/Entities/Image';
 import { Product } from '@/Entities/Product';
 import DashboardLayout from '@/Layouts/DashboardLayout';
+import CloseIcon from '@mui/icons-material/Close';
+import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Backdrop from '@mui/material/Backdrop';
 import Box from '@mui/material/Box';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Fab from '@mui/material/Fab';
+import IconButton from '@mui/material/IconButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Stack from '@mui/material/Stack';
+import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
 import dayjs from 'dayjs';
 import React from 'react';
@@ -18,6 +29,11 @@ type TPropsShowProduct = {
 }
 
 export default function ShowProduct({ product }: TPropsShowProduct) {
+  const [photoBackdrop, setPhotoBackdrop] = React.useState<Image | null>(null);
+
+  const [anchorElOptions,
+    setAnchorElOptions] = React.useState<null | HTMLElement>(null);
+
   const isPhotosExists = React.useMemo<boolean>(() => {
     if (!product?.photos) {
       return false;
@@ -68,18 +84,24 @@ export default function ShowProduct({ product }: TPropsShowProduct) {
           <Carousel
             animation="slide"
             indicators={false}
+            autoPlay={!photoBackdrop}
           >
             {product.photos?.map((photo) => (
-              <img
+              <Box
                 key={photo.id}
-                src={photo.url}
-                alt={photo.caption}
-                loading="lazy"
-                style={{
-                  width: '100%',
-                  objectFit: 'cover',
-                }}
-              />
+                onClick={() => setPhotoBackdrop(photo)}
+              >
+                <img
+                  src={photo.url}
+                  alt={photo.caption}
+                  loading="lazy"
+                  style={{
+                    width: '100%',
+                    objectFit: 'cover',
+                  }}
+                  title="Click to open full screen"
+                />
+              </Box>
             ))}
           </Carousel>
         )}
@@ -112,6 +134,92 @@ export default function ShowProduct({ product }: TPropsShowProduct) {
           ]}
         />
       </Box>
+
+      <Backdrop
+        sx={{
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+          backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        }}
+        open={Boolean(photoBackdrop)}
+      >
+        {photoBackdrop && (
+          <Box
+            sx={{
+              width: '100%',
+              height: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <img
+              src={photoBackdrop.url}
+              alt={photoBackdrop.caption}
+              loading="lazy"
+              style={{
+                width: '100vw',
+                height: '100vh',
+                objectFit: 'contain',
+              }}
+            />
+
+            <Stack
+              direction="row"
+              spacing={2}
+              sx={{
+                position: 'absolute',
+                top: 16,
+                right: 16,
+              }}
+            >
+              {/* Option Menu */}
+              <Tooltip title="Options" arrow>
+                <IconButton
+                  aria-label="options"
+                  onClick={(event) => setAnchorElOptions(event.currentTarget)}
+                  sx={{
+                    color: (theme) => theme.palette.grey[300],
+                  }}
+                >
+                  <MoreVertIcon />
+                </IconButton>
+              </Tooltip>
+
+              <Tooltip title="Close" arrow>
+                <IconButton
+                  aria-label="close"
+                  onClick={() => setPhotoBackdrop(null)}
+                  sx={{
+                    color: (theme) => theme.palette.grey[300],
+                  }}
+                >
+                  <CloseIcon />
+                </IconButton>
+              </Tooltip>
+            </Stack>
+
+            <Menu
+              id="options-menu"
+              anchorEl={anchorElOptions}
+              open={Boolean(anchorElOptions)}
+              onClose={() => setAnchorElOptions(null)}
+              MenuListProps={{
+                'aria-labelledby': 'options-menu',
+              }}
+            >
+              {/* TODO: handle delete action */}
+              <MenuItem>
+                <ListItemIcon>
+                  <DeleteIcon fontSize="small" />
+                </ListItemIcon>
+                <Typography variant="inherit">
+                  Delete Photo
+                </Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
+        )}
+      </Backdrop>
 
       <Link
         href={route('products.edit', { product })}
