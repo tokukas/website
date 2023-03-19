4b3e82a63d749ecb3a074f9f1d76dfb6d14e3e41
Fityan <fityannugroho@gmail.com>
2023-03-19 20:37:01 +0700

    feat: add product list in homepage

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ProductResource;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $products = ProductResource::collection(
+            \App\Models\Product::with('photos')->offset(0)->limit(10)->get()
+        );
+        $products->withoutWrapping();
+
+        $defaultProductPhoto = config('asset.default_product_photo');
+
+        return Inertia::render('Home', [
+            'photoPlaceholder' => $defaultProductPhoto ? url('/images/' . $defaultProductPhoto) : null,
+            'products' => $products,
+        ]);
+    }
+}
---
+++ resources/scripts/Pages/Home.tsx
@@ -0,0 +1,110 @@
+import AppHead from '@/Components/AppHead';
+import Link from '@/Components/Link';
+import Navbar from '@/Components/Navbar';
+import DashboardMenu from '@/Components/NavMenuItems/DashboardMenu';
+import LoginMenu from '@/Components/NavMenuItems/LoginMenu';
+import RegisterMenu from '@/Components/NavMenuItems/RegisterMenu';
+import SettingsMenu from '@/Components/NavMenuItems/SettingsMenu';
+import { Product } from '@/Entities/Product';
+import BaseLayout from '@/Layouts/BaseLayout';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+import route from 'ziggy-js';
+
+export type TPropsWelcome = {
+  photoPlaceholder: string;
+  products: readonly Product[];
+}
+
+export default function Welcome({ photoPlaceholder, products }: TPropsWelcome) {
+  return (
+    <>
+      <AppHead
+        title="Tokukas - Toko Buku Bekas"
+        description="Tokukas adalah tempat jual beli buku bekas berkualitas
+          dengan harga terjangkau. #YangBekasPastiLebihMurah"
+      />
+      <Navbar
+        setMainUserMenus={(isUserAuthenticated) => (isUserAuthenticated
+          ? [
+            DashboardMenu,
+            SettingsMenu,
+          ] : [
+            LoginMenu,
+            RegisterMenu,
+          ]
+        )}
+      />
+
+      <Container
+        sx={{
+          paddingTop: '72px',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(auto-fill, 160px)',
+            rowGap: 3,
+            columnGap: 2,
+            justifyContent: 'center',
+            mt: 4,
+          }}
+        >
+          {products.map((product) => (
+            <Card
+              key={product.id}
+              sx={{ width: 160 }}
+              elevation={3}
+            >
+              <CardActionArea
+                LinkComponent={Link}
+                href={route('products.show', product)}
+                sx={{ height: '100%' }}
+              >
+                <CardMedia
+                  sx={{ width: 160, height: 160 }}
+                  image={product.photos?.at(0)?.url ?? photoPlaceholder}
+                  title={product.name}
+                />
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    sx={{
+                      overflow: 'hidden',
+                      textOverflow: 'ellipsis',
+                      display: '-webkit-box',
+                      WebkitLineClamp: 2,
+                      WebkitBoxOrient: 'vertical',
+                    }}
+                    title={product.name}
+                  >
+                    {product.name}
+                  </Typography>
+                  <Typography color="text.primary" fontWeight="bold">
+                    {`Rp${product.price}`}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+        </Box>
+      </Container>
+    </>
+  );
+}
+
+/**
+ * Set the parent layout for this page.
+ *
+ * @see https://inertiajs.com/pages#persistent-layouts
+ */
+Welcome.layout = (children: React.ReactNode) => (
+  <BaseLayout>{children}</BaseLayout>
+);
