51e5bbccbdeca262e36780ef81859391090752d7
Fityan <fityannugroho@gmail.com>
2023-03-23 21:49:55 +0700

    feat: create global flash status

--- resources/scripts/Layouts/BaseLayout.tsx
+++ resources/scripts/Layouts/BaseLayout.tsx
@@ -1,14 +1,34 @@
+import DismissSnackbarAction from '@/Components/Snackbar/Action/Dismiss';
 import AuthContext, { AuthContextType } from '@/Utils/AuthContext';
 import { usePage } from '@inertiajs/react';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { useSnackbar } from 'notistack';
 import React from 'react';
 
 type TPropsBaseLayout = {
   children?: React.ReactNode;
 }
 
+type TPropsFlash = {
+  message: string | null;
+  status: 'success' | 'error' | 'warning' | null;
+}
+
 export default function BaseLayout({ children }: TPropsBaseLayout) {
+  const { enqueueSnackbar } = useSnackbar();
+  const { message, status } = usePage().props.flash as TPropsFlash;
+
+  React.useEffect(() => {
+    if (status) {
+      enqueueSnackbar(message ?? status, {
+        variant: status,
+        action: DismissSnackbarAction,
+        preventDuplicate: true,
+      });
+    }
+  }, [status, message]);
+
   return (
     <AuthContext.Provider
       value={usePage().props.auth as AuthContextType}
