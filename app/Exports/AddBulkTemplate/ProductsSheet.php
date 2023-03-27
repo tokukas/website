b45660019c8a57d489ffb3b5a7f187c76c7051ce
Fityan <fityannugroho@gmail.com>
2023-03-27 12:25:53 +0700

    feat: update products sheet in add bulk template

--- app/Exports/AddBulkTemplate/ProductsSheet.php
+++ app/Exports/AddBulkTemplate/ProductsSheet.php
@@ -3,20 +3,23 @@
 namespace App\Exports\AddBulkTemplate;
 
 use App\Exports\BasicSheet;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class ProductsSheet extends BasicSheet
+class ProductsSheet extends BasicSheet implements WithStyles
 {
     const columnKeys = [
-        'sku',
+        'book_id',
         'name',
-        'price',
+        'sku',
         'stock',
-        'description',
+        'price',
         'photo_1',
         'photo_2',
         'photo_3',
         'photo_4',
         'photo_5',
+        'description',
     ];
 
     protected string $name = 'Products';
@@ -29,16 +32,41 @@ class ProductsSheet extends BasicSheet
             'required',
             'required',
             'required',
-            'required',
             'optional',
             'optional',
             'optional',
             'optional',
+            'optional',
+            'optional',
+        ],
+        [
+            'Put id, or new book title. Invalid id will be recognized as new book title',
+            'Max. 70 characters',
+            'Max. 22 characters',
+            'Integer. Ex: 100',
+            'Integer. Ex: 10000',
+            'Valid image URL',
+            'Valid image URL',
+            'Valid image URL',
+            'Valid image URL',
+            'Valid image URL',
+            'Max. 255 characters',
         ],
     ];
 
     public function __construct()
     {
         $this->data = collect();
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            1 => [
+                'font' => [
+                    'bold' => true,
+                ],
+            ],
+        ];
     }
 }
